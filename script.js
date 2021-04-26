7ce8698045b49d0023dc2210776bddab5bcd7cdf
caleblaroche14 <69857383+caleblaroche14@users.noreply.github.com>
2021-04-26 16:06:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const ampm = document.getElementById("ampm");
+
+function settime(){
+    let time = new Date();
+    seconds.innerText = format(time.getSeconds());
+    minutes.innerText = format(time.getMinutes());
+
+    if (time.getHours() < 12){
+        hours.innerText = format(time.getHours());
+    }else{
+        hours.innerText = format((time.getHours() - 12));
+    }
+}
+
+function format(d){
+    if (d >= 10){
+        return d;
+    }else{
+        return '0'+d;
+    }
+}
+
+setInterval(settime,1000);
